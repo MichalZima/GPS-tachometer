219e766f05a7c6e171e3245e9f875368319feb49
MichalZima <michal.zima01@gmail.com>
2020-12-15 18:00:58 +0100

    modified

--- arduino/MAIN/main_ESP-12/MyTFT.h
+++ arduino/MAIN/main_ESP-12/MyTFT.h
@@ -1,18 +1,21 @@
 #include <SPI.h>
 #include <Adafruit_GFX.h>    // Core graphics library
 #include <Adafruit_ST7735.h> // Hardware-specific library for ST7735
+#include "Buttons.h"
 
 #define TFT_CS        D1
 #define TFT_RST       D0 // Or set to -1 and connect to Arduino RESET pin
 #define TFT_DC        D2
 
 Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
-
+Pushed pushed;
 
 class MyTFT {
   
   private:
     char convertString[10];
+    byte x0 = 1;
+    byte x1 = 1;
     
   public:  
     void tftSetup() {
@@ -41,5 +44,45 @@ class MyTFT {
       tft.setTextColor(TEXTCOLOR, ST7735_BLACK);
     }
 
+    void showMenu(){
+      Settings(1, 20, 10);
+      tft.print("session");
+      
+      Settings(1, 20, 25);
+      tft.print("zobrazenie");
+
+      Settings(1, 20, 40);
+      tft.print("ukladanie");
+
+      Settings(1, 20, 55);
+      tft.print("statistiky");
+
+      Settings(1, 20, 70);
+      tft.print("wifi");
+
+      Settings(1, 20, 85);
+      tft.print("casove pasmo");
+
+      Settings(1, 20, 100);
+      tft.print("pamat");
+
+      Settings(1, 20, 115);
+      tft.print("reset");
+
+      Settings(1, 20, 130);
+      tft.print("exit");
+    }
+
+     void Cursor(){
+      x1 = pushed.state*15-5;
+      
+      if(x0 != x1){
+        Settings(1, 10, x0);
+        tft.print(" ");
+        Settings(1, 10, x1);
+        tft.print(">");
+        x0 = x1;
+      }
+    }
 
 };
