c4e3d27884d7b37b569d48668a29c65d5fbcf0a3
MichalZima <michal.zima01@gmail.com>
2020-12-18 13:44:19 +0100

    debugged

--- arduino/MAIN/main_ESP-12/main_ESP-12.ino
+++ arduino/MAIN/main_ESP-12/main_ESP-12.ino
@@ -1,12 +1,12 @@
 #include "mySD.h"
 #include "Menu.h"
 
-byte Loops =0;
+byte Loops = 0;
 unsigned long Millis0 = 0;
 bool timePassed = true;
 float Hdop = 0;
-float speeds = {0,0,0,0,0,0,0,0,0,0,0};
-float hdops = {0,0,0,0,0,0,0,0,0,0,0};
+float speeds[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+float hdops[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 float averageSpeed = 0;
 float averageHdop = 0;
 byte arrayPosition = 0;
@@ -20,10 +20,10 @@ Menu menu;
 
 
 
-void setup(){
+void setup() {
   SD.begin(D8);
   Serial.begin(74880);
-  myGPS.gpsSetup();  
+  myGPS.gpsSetup();
   myTFT.tftSetup();
   pushed.buttonsSetup();
 }
@@ -35,34 +35,34 @@ void setup(){
 
 
 
-void loop() { 
-  
-  if(pushed.menuState == 0){                                    //switch to main screen
-    
+void loop() {
+
+  if (pushed.menuState == 0) {                                  //switch to main screen
+
     pushed.maxState = 4;
     myTFT.Settings(1, 40, 40);
     myTFT.Print(Hdop, 4, 1);
-    
+
     switch (pushed.state) {
-      
+
       case 1:
-        
-        //if (Speed < 3 || Hdop > 12) Speed = 0; 
+
+        //if (Speed < 3 || Hdop > 12) Speed = 0;
         myTFT.Settings(2, 10, 10);
         myTFT.Print(myGPS.Speed, 4, 1);
         tft.setTextSize(1);
         tft.print(" km/h");
         break;
-      
+
       case 2:
-        
+
         if (myGPS.totalDistance < 1000) {
           myTFT.Settings(2, 10, 10);
           myTFT.Print(myGPS.totalDistance, 3, 0);
           tft.setTextSize(1);
-          tft.print(" m");  
+          tft.print(" m");
         }
-        
+
         else if (myGPS.totalDistance >= 1000) {
           myTFT.Settings(2, 10, 10);
           myTFT.Print(myGPS.totalDistance, 7, 2);
@@ -70,21 +70,21 @@ void loop() {
           tft.print(" km");
         }
         break;
-     
+
       case 3:
-        
+
         myTFT.Settings(3, 10, 10);
         tft.print("screen 3");
         break;
-      
+
       case 4:
-       
+
         myTFT.Settings(3, 0, 80);
         tft.print("screen 4");
         break;
-      
+
       default:
-        
+
         myTFT.Settings(3, 10, 10);
         tft.print("default");
         break;
@@ -92,34 +92,38 @@ void loop() {
   }
 
 
-  else if(pushed.menuState == 1){                               //switching to menu
+  else if (pushed.menuState == 1) {                             //switching to menu
     pushed.maxState = 8;
     menu.Cursor();
     menu.showMenu();
   }
 
-  else if(pushed.menuState == 2){                               //select between options in menu
+  else if (pushed.menuState == 2) {                             //select between options in menu
     menu.select();
   }
 
-  if (Loops >= 5){
-    
-    if (Hdop < 50){                                               //saving data
+  if (Loops >= 5) {
+
+    if (Hdop < 50) {                                              //saving data
       passTime();
-      
-      if (myGPS.position0Saved == false && timePassed == true){
+
+      if (myGPS.position0Saved == false && timePassed == true) {
         Serial.print("     saved");
         myGPS.savePosition0();
         myGPS.distanceMeasurements++;
-  
-        if (myGPS.Speed > 6) {Millis0 = millis() + 1000;}              
-        
-        else if (myGPS.Speed > 2 && myGPS.Speed < 6)  {Millis0 = millis() + 2000;}
-  
+
+        if (myGPS.Speed > 6) {
+          Millis0 = millis() + 1000;
+        }
+
+        else if (myGPS.Speed > 2 && myGPS.Speed < 6)  {
+          Millis0 = millis() + 2000;
+        }
+
         saveToArray();
         arrayPosition++;
-                    
-        if(myGPS.distanceMeasurements >= 10 || course0 + 30 >= gps.course.deg() || course0 - 30 =< gps.course.deg()){
+
+        if (myGPS.distanceMeasurements >= 10 || myGPS.course0 + 30 >= gps.course.deg() || myGPS.course0 - 30 <= gps.course.deg()) {
           calculateAverage();
           mySD.savePosition();
           mySD.saveData(averageSpeed, averageHdop);
@@ -127,18 +131,18 @@ void loop() {
           myGPS.course0 = gps.course.deg();
           myGPS.distance = 0;
           arrayPosition = 0;
-          speeds = {0,0,0,0,0,0,0,0,0,0,0};
-          hdops = {0,0,0,0,0,0,0,0,0,0,0};
+          speeds = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+          hdops = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
         }
       }
     }
   }
 
 
-  if(!SD.begin(D8)) {
-      myTFT.Settings(1, 10, 90);
-      tft.print("sd fail");
-    }
+  if (!SD.begin(D8)) {
+    myTFT.Settings(1, 10, 90);
+    tft.print("sd fail");
+  }
   else {
     myTFT.Settings(1, 10, 90);
     tft.print("       ");
@@ -158,44 +162,43 @@ void loop() {
 
 
 
-void passTime(){
+void passTime() {
   if (Millis0 <= millis()) {
-    if (myGPS.position0Saved == true){
+    if (myGPS.position0Saved == true) {
       myGPS.distanceCalculating();
-      timePassed = true; 
+      timePassed = true;
     }
   }
-  else timePassed = false; 
+  else timePassed = false;
 }
 
 
 
-void clearScreen(){
-  if(pushed.menuState == 0 && pushed.nextPrevious() == true){
+void clearScreen() {
+  if (pushed.menuState == 0 && pushed.nextPrevious() == true) {
     tft.fillScreen(ST7735_BLACK);
   }
-  else if(pushed.confirm() == true){
+  else if (pushed.confirm() == true) {
     tft.fillScreen(ST7735_BLACK);
   }
 }
 
-byte calculateAverage() {
+void calculateAverage() {
   float speedsTogether = 0;
   float hdopsTogether = 0;
-  for(int i = 0; i < 11; i++){
-    if(speeds[i] != 0 && hdops[i] != 0){
+  for (int i = 0; i < 11; i++) {
+    if (speeds[i] != 0 && hdops[i] != 0) {
       speedsTogether = speedsTogether + speeds[i];
       hdopsTogether = hdopsTogether + hdops[i];
     }
-    else i = 15;  
+    else i = 15;
   }
   averageSpeed = speedsTogether / myGPS.distanceMeasurements;
   hdopsSpeed = hdopsTogether / myGPS.distanceMeasurements;
-  return 1;
 }
 
 
-void saveToArray(){
+void saveToArray() {
   speeds[arrayPosition] = gps.speed.kmph();
   hdops[arrayPosition] = gps.hdop.hdop();
 }
