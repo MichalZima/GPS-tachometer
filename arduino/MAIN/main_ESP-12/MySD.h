7b6d98a3805b156be7cd71f61fb5cf9696d0f674
MichalZima <michal.zima01@gmail.com>
2020-12-18 13:44:00 +0100

    debugged

--- arduino/MAIN/main_ESP-12/MySD.h
+++ arduino/MAIN/main_ESP-12/MySD.h
@@ -9,46 +9,45 @@ class MySD {
   private:
     char latString[9];
     char longString[9];
-    char 
-  
+
   public:
-    
-    void savePosition() { 
-      char fileName[32]; 
+
+    void savePosition() {
+      char fileName[32];
       sprintf(fileName, "coordinates/%02d.%02d.%02d.txt", gps.date.day(), gps.date.month(), gps.date.year());
       file = SD.open(fileName, FILE_WRITE);
-      if (file) { 
+      if (file) {
         dtostrf(gps.location.lat(), 9, 6, latString);
         dtostrf(gps.location.lng(), 9, 6, longString);
         file.print("[");
-        file.print(longString); 
-        file.print(", ");     
-        file.print(latString);    
-        file.println("], ");   
-        file.close();    
+        file.print(longString);
+        file.print(", ");
+        file.print(latString);
+        file.println("], ");
+        file.close();
       }
     }
 
     void saveData(float avSpeed, float avHdop) {
-      char fileName[32]; 
+      char fileName[32];
       sprintf(fileName, "data/%02d.%02d.%02d.txt", gps.date.day(), gps.date.month(), gps.date.year());
       file = SD.open(fileName, FILE_WRITE);
-      if (file) { 
+      if (file) {
         file.print(gps.time.value());
-        file.print("\t"); 
-        file.print(myGPS.distance);     
-        file.print("\t"); 
+        file.print("\t");
+        file.print(myGPS.distance);
+        file.print("\t");
         file.print(myGPS.totalDistance);
-        file.print("\t"); 
+        file.print("\t");
         file.print(gps.speed.kmph());
-        file.print("\t"); 
+        file.print("\t");
         file.print(avSpeed);
-        file.print("\t"); 
+        file.print("\t");
         file.print(gps.hdop.hdop());
-        file.print("\t"); 
-        file.print(avHdop);   
-        file.close();    
+        file.print("\t");
+        file.print(avHdop);
+        file.close();
       }
     }
-    
+
 };
