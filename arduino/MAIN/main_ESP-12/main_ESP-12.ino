fe5d6784a1911c9371f19fbe6473f27610d6544f
MichalZima <michal.zima01@gmail.com>
2020-12-28 13:38:43 +0100

    edit statement

--- arduino/MAIN/main_ESP-12/main_ESP-12.ino
+++ arduino/MAIN/main_ESP-12/main_ESP-12.ino
@@ -221,8 +221,8 @@ void printValuesForObservation(){
   myTFT.Settings(1, 10, 50);
   myTFT.Print(gps.hdop.hdop(), 4, 1);
   tft.print("hdop ");
-  if (gps.hdop.hdop() < 90 && gps.hdop.isUpdated()){
-    myTFT.Print(gps.hdop.age(), 6, 0);
+  myTFT.Print(gps.hdop.age(), 6, 0);
+  if (gps.hdop.hdop() < 90){
     myTFT.Settings(1, 10, 60);
     myTFT.Print(myGPS.distance0, 6, 2);
     tft.print("m ");
@@ -239,12 +239,16 @@ void printValuesForObservation(){
     tft.print("deg ");
     myTFT.Print(gps.course.age(), 6, 0);
     myTFT.Settings(1, 10, 100);
+  }
     myTFT.Print(gps.sentencesWithFix(), 5, 0);
     tft.print(" fixed");
     myTFT.Settings(1, 10, 110);
     myTFT.Print(gps.failedChecksum(), 5, 0);
     tft.print(" failed");
     myTFT.Settings(1, 10, 120);
+    myTFT.Print(gps.passedChecksum(), 5, 0);
+    tft.print(" passed");
+    myTFT.Settings(1, 10, 130);
     myTFT.Print(myGPS.distanceMeasurements, 2, 0);
     myTFT.Settings(1, 10, 130);
     tft.print(savedToSD);
