e85b8d1084a48a298b0f6af7b11c8ba62920ff24
MichalZima <michal.zima01@gmail.com>
2020-12-29 10:25:26 +0100

    moved printing main screens to Screens class

--- arduino/MAIN/main_ESP-12/Screens.h
+++ arduino/MAIN/main_ESP-12/Screens.h
@@ -11,69 +11,43 @@ Menu menu;
 class Screens {
   
   public:
-    void first() {
-      if (!SD.begin(D8)) {
-        myTFT.Settings(1, 80, 150);
-        tft.print("sd fail");
-      }
-  
-      if (pushed.menuState == 0 && pushed.nextPrevious() == true) {
-        tft.fillScreen(ST7735_BLACK);
-      } 
-  
-      pushed.maxState = 4;
-  
+    void First() {
+      myTFT.Settings(2, 10, 10);
+      myTFT.Print(gps.speed.kmph(), 4, 1);
+      tft.setTextSize(1);
+      tft.print(" km/h ");
+      myTFT.Print(gps.speed.age(), 5, 0);
       printValuesForObservation();
-  
-      switch (pushed.state) {
-  
-        case 1:
-  
-          //if (Speed < 3 || Hdop > 12) Speed = 0;
-          myTFT.Settings(2, 10, 10);
-          myTFT.Print(gps.speed.kmph(), 4, 1);
-          tft.setTextSize(1);
-          tft.print(" km/h ");
-          myTFT.Print(gps.speed.age(), 5, 0);
-          break;
-  
-        case 2:
-  
-          if (myGPS.changedToKM == false) {
-            myTFT.Settings(2, 10, 10);
-            myTFT.Print(myGPS.totalDistance, 3, 0);
-            tft.setTextSize(1);
-            tft.print(" m");
-          }
-  
-          else if (myGPS.changedToKM == true) {
-            myTFT.Settings(2, 10, 10);
-            myTFT.Print(myGPS.totalDistance, 7, 2);
-            tft.setTextSize(1);
-            tft.print(" km");
-          }
-          break;
-  
-        case 3:
-  
-          myTFT.Settings(3, 10, 10);
-          tft.print("screen 3");
-          break;
-  
-        case 4:
-  
-          myTFT.Settings(2, 10, 10);
-          tft.print(myGPS.realTime());
-          break;
-  
-        default:
-  
-          myTFT.Settings(3, 10, 10);
-          tft.print("default");
-          break;
-      }
     }
 
+    void Second() {
+      if (myGPS.changedToKM == false) {
+        myTFT.Settings(2, 10, 10);
+        myTFT.Print(myGPS.totalDistance, 3, 0);
+        tft.setTextSize(1);
+        tft.print(" m");
+      }
+
+      else if (myGPS.changedToKM == true) {
+        myTFT.Settings(2, 10, 10);
+        myTFT.Print(myGPS.totalDistance, 7, 2);
+        tft.setTextSize(1);
+        tft.print(" km");
+      }
+      printValuesForObservation();
+    }
+
+    void Third() {
+      myTFT.Settings(3, 10, 10);
+      tft.print("screen 3");
+      printValuesForObservation();
+    }
+
+    void Fourth() {
+      myTFT.Settings(2, 10, 10);
+      tft.print(myGPS.realTime());
+      printValuesForObservation();
+    }
 
 
   private:
@@ -118,5 +92,5 @@ class Screens {
       myTFT.Settings(1, 10, 130);
       tft.print(savedToSD);
     }
-  
+
 };
