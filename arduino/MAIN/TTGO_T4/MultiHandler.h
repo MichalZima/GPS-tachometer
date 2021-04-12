77b41434d36ac1a0bbbca4c147d3ebf3846b50fb
MichalZima <michal.zima01@gmail.com>
2021-04-12 19:35:38 +0200

    made multi handler

--- arduino/MAIN/TTGO_T4/MultiHandler.h
+++ arduino/MAIN/TTGO_T4/MultiHandler.h
@@ -1,53 +1,134 @@
-/////////////////////////////////////////////////////////////////
 
-#include "Button2.h";
+#include "Button2.h"
 
-/////////////////////////////////////////////////////////////////
 
-#define BUTTON_A_PIN  39
+class MultiHandler {
 
-/////////////////////////////////////////////////////////////////
 
-Button2 buttonA = Button2(BUTTON_A_PIN);
+  public:
+    byte menuState = 0;
+    byte maxState = 4;
+    byte state = 1;
+    unsigned long previousMillis = 0;
+    bool screenOff = false;
+    byte eventA;
+    byte eventB;
+    byte eventC;
 
-/////////////////////////////////////////////////////////////////
+/*------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/    
 
-void setup() {
-  Serial.begin(9600);
-  delay(50);
-  Serial.println("\n\nMulti Handler Demo");
-  
-  buttonA.setClickHandler(handler);
-  buttonA.setLongClickHandler(handler);
-  buttonA.setDoubleClickHandler(handler);
-  buttonA.setTripleClickHandler(handler);
-}
-
-/////////////////////////////////////////////////////////////////
-
-void loop() {
-  buttonA.loop();
-}
-
-/////////////////////////////////////////////////////////////////
-
-void handler(Button2& btn) {
-    switch (btn.getClickType()) {
+    void buttonHandlerA(Button2& btn) {
+      switch (btn.getClickType()) {
         case SINGLE_CLICK:
+            if (!screenOff) {
+              state++;
+              if (state > maxState) state = 1;
+            }
+            
+            if (screenOff) {
+              pinMode (4, OUTPUT);
+              screenOff = false;
+            }
+            
+            previousMillis = millis();
+            eventA = 1;
             break;
+            
         case DOUBLE_CLICK:
-            Serial.print("double ");
+            eventA = 2;
             break;
         case TRIPLE_CLICK:
-            Serial.print("triple ");
+            eventA = 3;
             break;
         case LONG_CLICK:
-            Serial.print("long");
+            eventA = 4;
             break;
+      }
     }
-    Serial.print("click");
-    Serial.print(" (");
-    Serial.print(btn.getNumberOfClicks());    
-    Serial.println(")");
-}
-/////////////////////////////////////////////////////////////////
+    
+/*------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+    
+    void buttonHandlerB(Button2& btn) {
+      switch (btn.getClickType()) {
+        case SINGLE_CLICK:
+            if (!screenOff) {
+              state--;
+              if (state < 1) state = maxState;
+            }
+            
+            if (screenOff) {
+              pinMode (4, OUTPUT);
+              screenOff = false;
+            }
+            
+            previousMillis = millis();
+            eventB = 1;
+            break;
+            
+        case DOUBLE_CLICK:
+            eventB = 2;
+            break;
+        case TRIPLE_CLICK:
+            eventB = 3;
+            break;
+        case LONG_CLICK:
+            eventA = 4;
+            break;
+            
+      }
+    }
+    
+/*------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+    
+    void buttonHandlerC(Button2& btn) {
+      switch (btn.getClickType()) {
+        case SINGLE_CLICK:
+          if (!screenOff) {
+            if (menuState == 0) {
+              menuState = 1;
+              state = 1;
+            }
+            
+            else if (menuState == 1) {
+              menuState = 2;
+            }
+          }
+          
+          if (screenOff) {
+            pinMode (4, OUTPUT);
+            screenOff = false;
+          }
+          
+          previousMillis = millis();
+          eventC = 1;
+          break;
+        
+       case DOUBLE_CLICK:
+            eventC = 2;
+            break;
+        
+        case TRIPLE_CLICK:
+            eventC = 3;
+            break;
+        case LONG_CLICK:
+            eventC = 4;
+            break;
+      }
+    }
+
+/*------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+    int changeSettingValue(int MIN, int MAX, int NOW){
+      int valueOfSetting = NOW;
+      if (eventA > 0) {
+        valueOfSetting++;
+        if (valueOfSetting > MAX) valueOfSetting = MIN;
+      }
+      else if (eventB > 0) {
+        valueOfSetting--;
+        if (valueOfSetting < MIN) valueOfSetting = MAX;
+      }
+      return valueOfSetting;
+    }
+
+};
